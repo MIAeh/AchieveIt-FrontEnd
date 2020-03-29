90af5f102bfe46d34c3c2aeb64ffff18db615eaf
Jan-code <839234349@qq.com>
2020-03-29 15:48:32 +0800

    update functionList-upload/download excel

--- src/views/projectInfo/functionList.vue
+++ src/views/projectInfo/functionList.vue
@@ -8,24 +8,46 @@
             <el-button type="primary" icon="el-icon-plus" @click="showCreateFunctionDialog">新建功能</el-button>
           </el-col>
           <el-col :span="3" :offset="14">
-            <el-button type="primary" icon="el-icon-upload2" style="float:right">导入</el-button>
+            <ele-import
+              :fields="importForm.fields"
+              :filepath="importForm.filepath"
+              :append="importForm.append"
+              :formatter="importForm.formatter"
+              :requestFn="requestFn"
+              :rules="importForm.rules"
+              :tips="importForm.tips"
+              :title="importForm.title"
+              :visible.sync="importForm.visible"
+              @close="handleCloseImport"
+              @finish="handleFinishImport"
+            />
+            <el-button
+              type="primary"
+              icon="el-icon-download"
+              style="float:right"
+              @click="importForm.visible = true"
+            >导入</el-button>
           </el-col>
           <el-col :span="3">
-            <el-button type="primary" icon="el-icon-download" style="float:right">导出</el-button>
+            <el-button
+              type="primary"
+              icon="el-icon-upload2"
+              style="float:right"
+              @click="handleDownloadExcel"
+            >导出</el-button>
           </el-col>
         </el-row>
         <el-table
           :data="functions"
           border
           style="width: 100%"
-          @cell-click="handleClickFunction"
+          @cell-click="getFunctionInfo"
           row-key="featureId"
           default-expand-all
           :tree-props="{children: 'allChildren'}"
         >
           <el-table-column prop="featureId" label="ID" :show-overflow-tooltip="true" width="300" />
           <el-table-column prop="featureName" label="标题" :show-overflow-tooltip="true" />
-          <!--          <el-table-column prop="project" label="所属项目" :show-overflow-tooltip="true" width="150" />-->
           <el-table-column fixed="right" label="操作" width="80">
             <template slot-scope="scope">
               <el-button
@@ -56,21 +78,36 @@
           <el-col :span="8">
             <el-form-item label="功能类型">
               <el-select v-model="newFunction.featureLevel" @change="handleFeatureLevelChange">
-                <el-option v-for="item in functionTypeList" :key="item.label" :label="item.label" :value="item.value"></el-option>
+                <el-option
+                  v-for="item in functionTypeList"
+                  :key="item.label"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item label="一级父功能">
               <el-select :disabled="firstFatherDisabled" v-model="newFunction.firstFather">
-                <el-option v-for="item in firstFatherList" :key="item.featureId" :label="item.featureName" :value="item.featureId"></el-option>
+                <el-option
+                  v-for="item in firstFatherList"
+                  :key="item.featureId"
+                  :label="item.featureName"
+                  :value="item.featureId"
+                ></el-option>
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item label="二级父功能">
               <el-select :disabled="secondFatherDisabled" v-model="newFunction.secondFather">
-                <el-option v-for="item in secondFatherList" :key="item.featureId" :label="item.featureName" :value="item.featureId"></el-option>
+                <el-option
+                  v-for="item in secondFatherList"
+                  :key="item.featureId"
+                  :label="item.featureName"
+                  :value="item.featureId"
+                ></el-option>
               </el-select>
             </el-form-item>
           </el-col>
@@ -109,54 +146,64 @@
 </template>
 
 <script>
-  import {createFeature, deleteFeature, getFeature} from "@/api/feature";
+import { createFeature, deleteFeature, getFeature } from "@/api/feature";
+import EleImport from "vue-ele-import";
 
 export default {
   name: "AddProject",
+  components: {
+    EleImport
+  },
   data() {
     return {
       activeTabName: "functionList",
-      functions: [
-        {
-          featureId: "54321-0001",
-          featureName: "一级功能1",
-          projectId: "54321",
-          featureDescription: "",
-          allChildren: [
-            {
-              featureId: "54321-0001-001",
-              featureName: "二级功能1-1",
-              projectId: "54321",
-              featureDescription: ""
-            },
-            {
-              featureId: "54321-0001-002",
-              featureName: "二级功能1-2",
-              projectId: "54321",
-              featureDescription: "",
-              allChildren: [
-                {
-                  featureId: "54321-0001-002-001",
-                  featureName: "二级功能1-2-1",
-                  projectId: "54321"
-                }
-              ]
-            }
+      functions: [],
+      importForm: {
+        // 弹窗的标题
+        title: "导入功能列表",
+        // 提示信息
+        tips: ["名字必填", "年龄必填", "年龄请填写数字"],
+        // 假如数据库中是`name`字段, 而Excel模板列是`名字`, 就需要写成 name: '名字'
+        // ele-import 内部会抛弃非定义在fields里的列
+        fields: {
+          name: "名字",
+          age: "年龄",
+          city: "所在城市"
+        },
+        // formatter起到替换数据的作用
+        formatter: {
+          // 可以是对象, 在发送请求时, '深圳' 将被替换成 1, '广州' 被替换成 2
+          city: {
+            1: "深圳",
+            2: "广州"
+          },
+          // 可以是函数, 在发送请求时, `age` 将加1, 例如 原数据是 19 -> 20
+          age: function(value, row) {
+            return value + 1;
+          }
+        },
+        // 附加数据, 在每条记录上都会加这两个字段和值
+        append: {
+          company: "腾讯",
+          leader: "小马哥"
+        },
+        // 参数校检, 和 element-ui 中 form表单中传递的rules一样, 都是使用的 async-validator 库
+        // https://element.eleme.cn/#/zh-CN/component/form#biao-dan-yan-zheng
+        rules: {
+          name: { type: "string", required: true, message: "名字必填" },
+          age: [
+            { type: "number", message: "年龄必须为数字" },
+            { required: true, message: "年龄必须填写" }
           ]
         },
-        {
-          featureId: "54321-0002",
-          featureName: "一级功能2",
-          projectId: "54321",
-          featureDescription: ""
-        },
-        {
-          featureId: "54321-0003",
-          featureName: "一级功能3",
-          projectId: "54321",
-          featureDescription: ""
-        }
-      ],
+        // Excel模板下载地址
+        // 注意, 只能是.xlsx的文件, .xls或者.cvs都会报错
+        filepath:
+          "https://raw.githubusercontent.com/dream2023/vue-ele-import/master/public/user.xlsx",
+        // 控制弹窗, 和dialog的visible一样
+        // https://element.eleme.cn/#/zh-CN/component/dialog
+        visible: false
+      },
       newFunction: {
         featureName: "",
         projectId: "",
@@ -169,11 +216,11 @@ export default {
         featureName: [{ required: true, message: "请输入标题" }]
       },
       functionInfo: {
-        featureId: "54321-0001",
-        featureName: "一级功能1",
-        projectId: "54321",
-        createTime: "2020-03-14",
-        featureDescription: "一级功能1的描述"
+        featureId: "",
+        featureName: "",
+        projectId: "",
+        createTime: "",
+        featureDescription: ""
       },
       functionTypeList: [
         {
@@ -198,7 +245,10 @@ export default {
       return this.newFunction.featureLevel === 0;
     },
     secondFatherDisabled() {
-      return this.newFunction.featureLevel === 0 || this.newFunction.featureLevel === 1;
+      return (
+        this.newFunction.featureLevel === 0 ||
+        this.newFunction.featureLevel === 1
+      );
     },
     firstFatherList() {
       return this.functions;
@@ -226,7 +276,7 @@ export default {
     handleTabRoute(tab) {
       this.$router.push(`/projectInfo/${tab.name}`);
     },
-    handleClickFunction(row, column) {
+    getFunctionInfo(row, column) {
       this.functionInfo = row;
       if (column.label !== "操作") this.functionInfoDialogVisible = true;
     },
@@ -245,16 +295,14 @@ export default {
       createFeature(this.newFunction).then(res => {
         this.createFunctionDialogVisible = false;
         this.getFeature();
-      })
+      });
     },
     deleteFeature(row) {
       deleteFeature(row.featureId).then(res => {
         this.getFeature();
-      })
+      });
     },
-    updateFeature() {
-
-    },
+    updateFeature() {},
     handleFeatureLevelChange(value) {
       if (value === 0) {
         this.newFunction.firstFather = "";
@@ -263,6 +311,61 @@ export default {
         this.newFunction.secondFather = "";
       }
     },
+    async requestFn(data) {
+      // 演示代码
+      // 1、如果没有针对ele-import做过接口约定, 可以采用如下形式:
+      // try {
+      //   const res = await axios.post('/user', data)
+      //   return Promise.resolve()
+      // } catch (error) {
+      //   // error经过一系列转化, 转为
+      //   const errorData = this.getErrorMessage(error)
+      //   {0:{ age: '年龄为数字', city: '城市必填'}, 2:{ age: '年龄为数字'} } // 0 和 2 分别是行号
+      //   return Promise.reject(errorData)
+      // }
+      // 2、如果针对ele-import做过接口约定, 当校检错误时, 后端返回的 error 就是上述错误形式, 则可直接
+      // return axios.post('/user', data)
+      // 总结: 无论如何总要返回一个Promise对象
+    },
+    handleCloseImport() {
+      console.log("弹窗关闭了~");
+    },
+    handleFinishImport() {
+      console.log("导入完毕了~");
+    },
+    handleDownloadExcel() {
+      this.downloadLoading = true;
+      import("@/vendor/Export2Excel").then(excel => {
+        const tHeader = [ "项目ID","功能ID", "标题", "功能描述"];
+        const filterVal = [
+          "projectId",
+          "featureId",
+          "featureName",
+          "featureDescription"
+        ];
+        const list = this.functions;
+        const data = this.formatJson(filterVal, list);
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: '功能列表',
+          autoWidth: true,
+          bookType: 'xlsx'
+        });
+        this.downloadLoading = false;
+      });
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v =>
+        filterVal.map(j => {
+          if (j === "timestamp") {
+            return parseTime(v[j]);
+          } else {
+            return v[j];
+          }
+        })
+      );
+    }
   }
 };
 </script>
