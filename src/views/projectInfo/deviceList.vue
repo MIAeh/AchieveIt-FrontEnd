8aec9c53146a91209c2ccb02a2222bf21b6bcf7c
vast00 <372720586@qq.com>
2020-04-03 17:53:39 +0800

    finish device api

--- src/views/projectInfo/deviceList.vue
+++ src/views/projectInfo/deviceList.vue
@@ -15,19 +15,19 @@
         <el-table :data="deviceList" border style="width: 100%">
           <el-table-column prop="deviceID" label="设备ID" />
           <el-table-column prop="deviceType" label="设备类别" width="200" />
-          <el-table-column prop="deviceUser" label="使用者" width="160" />
-          <el-table-column prop="deviceDate" label="设备使用期限" width="160" />
+          <el-table-column prop="userName" label="使用者" width="160" />
+          <el-table-column prop="dueDate" label="设备使用期限" width="160" />
           <el-table-column prop="deviceStatus" label="设备状态" width="160">
             <template slot-scope="scope">
-              <el-tag type="success" v-if="scope.row.deviceStatus === '使用中'">使用中</el-tag>
-              <el-tag type="info" v-else-if="scope.row.deviceStatus === '已归还'">已归还</el-tag>
-              <el-tag type="danger" v-else>已逾期</el-tag>
+              <el-tag type="success" v-if="scope.row.deviceStatus === 'Using'">使用中</el-tag>
+              <el-tag type="info" v-else-if="scope.row.deviceStatus === 'Returned'">已归还</el-tag>
+              <el-tag type="danger" v-else-if="scope.row.deviceStatus === 'Overdue'">已逾期</el-tag>
             </template>
           </el-table-column>
           <el-table-column fixed="right" label="操作" width="120">
             <template slot-scope="scope">
               <el-button
-                @click.native.prevent="handleReturn(scope.$index)"
+                @click.native.prevent="handleReturn(scope.row)"
                 type="text"
                 size="small"
               >归还</el-button>
@@ -45,80 +45,75 @@
             <el-select
               v-model="addFrom.deviceID"
               filterable
-              remote
               reserve-keyword
               placeholder="请搜索设备"
-              :remote-method="handleDeviceSearch"
             >
               <el-option
                 v-for="item in deviceSearchList"
-                :key="item.id"
-                :label="item.label"
-                :value="item.id"
+                :key="item"
+                :label="item"
+                :value="item"
               ></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="设备使用者">
             <el-select v-model="addFrom.deviceUserID" placeholder="请选择设备使用者">
-              <el-option v-for="item in deviceUserList" :key="item" :label="item" :value="item"></el-option>
+              <el-option v-for="item in memberList" :key="item.memberID" :label="item.memberName" :value="item.memberID"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="设备使用期限">
-            <el-date-picker type="date" placeholder="选择日期" v-model="addFrom.deviceDate"></el-date-picker>
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              value-format="yyyy-MM-dd"
+              v-model="addFrom.deviceDate" />
           </el-form-item>
         </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click.native="addFromVisible = false">取消</el-button>
-        <el-button type="primary" @click="addSubmit">提交</el-button>
+        <el-button type="primary" @click="registerDevice">提交</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
+  import {getDeviceIDList, getDeviceList, registerDevice, returnDevice} from "@/api/device";
+  import {getMembers} from "@/api/Member";
+
 export default {
   data() {
     return {
       activeTabName: "deviceList",
       deviceList: [
-        {
-          deviceID: "PC-123123123123",
-          deviceType: "PC",
-          deviceUser: "小明",
-          deviceDate: "2020-20-20",
-          deviceStatus: "使用中"
-        },
-        {
-          deviceID: "PHONE-123123123123",
-          deviceType: "PHONE",
-          deviceUser: "小明",
-          deviceDate: "2020-20-20",
-          deviceStatus: "已归还"
-        },
-        {
-          deviceID: "PAD-123123123123",
-          deviceType: "PAD",
-          deviceUser: "小明",
-          deviceDate: "2020-20-20",
-          deviceStatus: "已逾期"
-        }
+        // {
+        //   deviceID: "PC-123123123123",
+        //   deviceType: "PC",
+        //   deviceUser: "小明",
+        //   deviceDate: "2020-20-20",
+        //   deviceStatus: "使用中"
+        // },
+        // {
+        //   deviceID: "PHONE-123123123123",
+        //   deviceType: "PHONE",
+        //   deviceUser: "小明",
+        //   deviceDate: "2020-20-20",
+        //   deviceStatus: "已归还"
+        // },
+        // {
+        //   deviceID: "PAD-123123123123",
+        //   deviceType: "PAD",
+        //   deviceUser: "小明",
+        //   deviceDate: "2020-20-20",
+        //   deviceStatus: "已逾期"
+        // }
       ],
       deviceSearchList: [
-        {
-          id: "PC-12312",
-          label: "设备1"
-        },
-        {
-          id: "PC-123123123123",
-          label: "设备2"
-        },
-        {
-          id: "PC-123123",
-          label: "设备3"
-        }
+        "PAD-123123123123",
+        "PHONE-123123123123"
       ],
-      deviceUserList: ["小明", "小刘", "小五"],
+      memberList: [],
       addFromVisible: false,
       addFrom: {
         deviceID: "",
@@ -127,26 +122,62 @@ export default {
       }
     };
   },
+  created() {
+    this.getDeviceList();
+  },
   methods: {
+    getDeviceList() {
+      getDeviceList(this.$store.state.project.currentProjectId).then(res => {
+        const { data } = res;
+        this.deviceList = data;
+      })
+    },
     handleTabRoute(tab, event) {
       this.$router.push(`/projectInfo/${tab.name}`);
     },
-    handleReturn(index) {
+    handleReturn(row) {
       this.$confirm("确认归还设备吗?", "提示", {
         type: "warning"
       })
         .then(() => {
-          this.deviceList[index].deviceStatus = "已归还";
+          returnDevice(
+            this.$store.state.project.currentProjectId,
+            row.userID,
+            row.deviceID
+          ).then(res => {
+            this.getDeviceList();
+          })
         })
         .catch(() => {});
     },
-    handleDeviceSearch() {},
     handleAddDevice() {
+      this.getDeviceIDList();
+      this.getMemberList();
       this.addFromVisible = true;
       this.addFrom = {};
     },
-    addSubmit() {
-      this.addFromVisible = false;
+    getDeviceIDList() {
+      getDeviceIDList().then(res => {
+        const { data } = res;
+        this.deviceSearchList = data;
+      })
+    },
+    getMemberList() {
+      getMembers(this.$store.state.project.currentProjectId, -1).then(res => {
+        const { data } = res;
+        this.memberList = data;
+      })
+    },
+    registerDevice() {
+      registerDevice(
+        this.$store.state.project.currentProjectId,
+        this.addFrom.deviceUserID,
+        this.addFrom.deviceID,
+        this.addFrom.deviceDate
+      ).then(res => {
+        this.addFromVisible = false;
+        this.getDeviceList();
+      })
     }
   }
 };
