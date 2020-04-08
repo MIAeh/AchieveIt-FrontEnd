6f08e4c2a6f2b49e24f3e69e4469f84d08f19cb4
Jan-code <839234349@qq.com>
2020-04-08 16:08:25 +0800

    projectList-getAllproject

--- src/views/projectList/index.vue
+++ src/views/projectList/index.vue
@@ -79,18 +79,7 @@ export default {
         size: 10,
         searchCondition: ""
       },
-      allProject: [
-        {
-          projectID: "2019-0000-D-01",
-          projectName: "test project updated 1",
-          projectStatus: 0,
-          projectManagerName: "boss",
-          projectMonitorName: "tester",
-          projectClientContactName: "billy",
-          projectStartDate: "2019-11-11 00:00:00",
-          projectEndDate: "2021-11-11 00:00:00"
-        }
-      ],
+      allProject: [],
       underwayProject: [],
       startProject: [],
       finishedProject: []
@@ -126,10 +115,10 @@ export default {
       }
     },
     getAllProject() {
-      // getProjectList("", -1).then(response => {
-      //   const { data } = response
-      //   this.allProject = data
-      // })
+      getProjectList("", -1).then(response => {
+        const { data } = response
+        this.allProject = data
+      })
     },
     getStartProject() {
       getProjectList("", 0).then(response => {
